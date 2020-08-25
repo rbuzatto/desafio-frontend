d0ed0ea462dbc3bad733240a4e994709c9c569c5
Rafael Buzatto de Campos <mightyrafa@gmail.com>
2020-08-25 20:33:49 -0300

    add navbar for routing

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,52 +1,171 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
+import ClickAwayListener from '@material-ui/core/ClickAwayListener'
 import MenuIcon from '@material-ui/icons/Menu'
-import Button from '@material-ui/core/Button'
-import { NavLink as Link } from 'react-router-dom'
+import Close from '@material-ui/icons/Close'
+import Link from '@material-ui/core/Link'
+import classNames from 'classnames/bind'
+import { NavLink as RouterLink } from 'react-router-dom'
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
+const linksValues = [
+  {
+    to: '/products',
+    display: 'Products',
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
+  {
+    to: '/top-products',
+    display: 'Top5',
   },
-  title: {
-    flexGrow: 1,
+  {
+    to: '/cart',
+    display: 'Cart',
   },
-}))
+]
+
 const NavBar = () => {
   const classes = useStyles()
+  const cx = classNames.bind(classes)
+  const [menuOpen, setMenuOpen] = useState(false)
+  let anchorElm = null
+
+  const toggleMenu = () => setMenuOpen(menuOpen => !menuOpen)
+  const closeMenu = () => setMenuOpen(false)
+  const handleClickAway = event => {
+    if (anchorElm.isSameNode(event.target) || anchorElm.contains(event.target)) return
+    setMenuOpen(false)
+  }
+
+  const renderLinks = () =>
+    linksValues.map(({ to, display }, idx) => (
+      <Link
+        key={idx}
+        component={RouterLink}
+        onClick={closeMenu}
+        activeStyle={{ color: '#ff46e7' }}
+        style={{ transitionDelay: `${idx * 40}ms` }}
+        className={cx(classes.menuLink, { [classes.menuLinkShow]: menuOpen })}
+        to={to}
+        color="inherit"
+      >
+        {display}
+      </Link>
+    ))
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+          <IconButton
+            buttonRef={node => (anchorElm = node)}
+            edge="start"
+            onClick={toggleMenu}
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <Close className={cx({ [classes.hide]: !menuOpen })} />
+            <MenuIcon className={cx({ [classes.hide]: menuOpen })} />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            <Button component={Link} to="/">
+            <Link component={RouterLink} to="/">
               Home
-            </Button>
+            </Link>
           </Typography>
-          <Button component={Link} to="/products">
-            Products
-          </Button>
-          <Button component={Link} to="/top-products">
-            Top5
-          </Button>
-          <Button component={Link} to="/cart">
-            Cart
-          </Button>
+          <ClickAwayListener onClickAway={handleClickAway}>
+            <div
+              id="menu-list"
+              className={cx(classes.menuList, { [classes.menuListShow]: menuOpen })}
+            >
+              {renderLinks()}
+            </div>
+          </ClickAwayListener>
         </Toolbar>
       </AppBar>
     </div>
   )
 }
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    position: 'relative',
+    top: 0,
+    width: '100vw',
+    height: '56px',
+    [theme.breakpoints.up('sm')]: {
+      height: '64px',
+    },
+  },
+  logo: {
+    flexGrow: 1,
+    '&:hover': {
+      textDecoration: 'none',
+    },
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  hide: {
+    display: 'none',
+  },
+  menuLink: {
+    marginLeft: 12,
+    '&:hover': {
+      textDecoration: 'none',
+      color: '#4dfeff',
+    },
+    [theme.breakpoints.down('xs')]: {
+      padding: '.8rem',
+      opacity: 0,
+      transform: 'translateX(-50%)',
+      margin: 0,
+      background: '#3a5577',
+      '&:hover': {
+        background: '#3988c3',
+      },
+
+      '&:not(:first-child)': {
+        borderTop: '1px solid #92b791',
+      },
+    },
+  },
+  menuLinkShow: {
+    [theme.breakpoints.down('xs')]: {
+      opacity: 1,
+      transform: 'translateX(0)',
+      transition: `
+          opacity .3s ease-in,
+          transform .3s ease-in,
+          background .10s linear,
+          color .10s linear
+          `,
+    },
+  },
+  menuList: {
+    marginLeft: 'auto',
+    [theme.breakpoints.down('xs')]: {
+      position: 'fixed',
+      flexDirection: 'column',
+      top: 50,
+      left: 20,
+      borderRadius: '4px',
+      width: '0',
+      display: 'flex',
+      overflow: 'hidden',
+    },
+  },
+  menuListShow: {
+    [theme.breakpoints.down('xs')]: {
+      width: '15rem',
+    },
+  },
+}))
 
 export default NavBar
