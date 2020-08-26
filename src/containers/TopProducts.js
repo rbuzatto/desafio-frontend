a40bd0a477e2488865b876aa3f884a6229017c28
Rafael Buzatto de Campos <mightyrafa@gmail.com>
2020-08-26 12:27:37 -0300

    display top products

--- src/containers/TopProducts.js
+++ src/containers/TopProducts.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import ProductCardList from 'components/ProductCardList'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
 
 const TopProducts = () => {
-  return <div>TopProducts</div>
+  const classes = useStyles()
+  return (
+    <>
+      <Typography className={classes.pageHeader} variant="h4" color="textSecondary">
+        Here are the exclusive Top 5!
+      </Typography>
+      <ProductCardList count={5} filter="unitPrice" order="desc" />
+    </>
+  )
 }
+
+const useStyles = makeStyles(() => ({
+  pageHeader: {
+    borderBottom: '1px solid #e8e8e8',
+    paddingBottom: 8,
+    marginBottom: 24,
+  },
+}))
 
 export default TopProducts
