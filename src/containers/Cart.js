2e504596ea6127eadb2625e64aa38a108106c5d3
Rafael Buzatto de Campos <mightyrafa@gmail.com>
2020-08-26 10:34:44 -0300

    add Header to cart

--- src/containers/Cart.js
+++ src/containers/Cart.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import ItemCardList from 'components/ItemCardList'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
 
 const Cart = () => {
-  return <ItemCardList />
+  const classes = useStyles()
+  return (
+    <>
+      <Typography className={classes.pageHeader} variant="h4" color="textSecondary">
+        Your Cart Items
+      </Typography>
+      <ItemCardList />
+    </>
+  )
 }
+
+const useStyles = makeStyles(() => ({
+  pageHeader: {
+    borderBottom: '1px solid #e8e8e8',
+    paddingBottom: 8,
+  },
+}))
 
 export default Cart
