e5c25813aab00e213a7f92c28bf12b8e21e8515a
Rafael Buzatto de Campos <mightyrafa@gmail.com>
2020-08-26 01:27:31 -0300

    create cart context

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
-import React, { createContext } from 'react'
+import React, { createContext, useReducer } from 'react'
 import NavBar from 'containers/NavBar'
 import Home from 'containers/Home'
 import Cart from 'containers/Cart'
 import TopProducts from 'containers/TopProducts'
 import Products from 'containers/Products'
 import useProducts from 'hooks/useProducts'
+import cartReducer, { initialState as cartState } from 'reducers/cart'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 export const ProductsContext = createContext(null)
+export const CartContext = createContext(null)
 
 const App = () => {
   const [products] = useProducts()
+  const [cart, dispatch] = useReducer(cartReducer, cartState)
 
   return (
     <ProductsContext.Provider value={{ products }}>
-      <Router>
-        <div className="app">
-          <NavBar />
-          <div className="container page">
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/products">
-                <Products />
-              </Route>
-              <Route path="/top-products">
-                <TopProducts />
-              </Route>
-              <Route path="/cart">
-                <Cart />
-              </Route>
-            </Switch>
+      <CartContext.Provider value={{ cart, dispatch }}>
+        <Router>
+          <div className="app">
+            <NavBar />
+            <div className="container page">
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/products">
+                  <Products />
+                </Route>
+                <Route path="/top-products">
+                  <TopProducts />
+                </Route>
+                <Route path="/cart">
+                  <Cart />
+                </Route>
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </CartContext.Provider>
     </ProductsContext.Provider>
   )
 }
