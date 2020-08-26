2f3356992f2ec8158335d12be7b5c78c749d8291
Rafael Buzatto de Campos <mightyrafa@gmail.com>
2020-08-26 01:27:10 -0300

    add action to add product to basket

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
@@ -8,10 +8,16 @@ import CardActions from '@material-ui/core/CardActions'
 import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
 import AddIcon from '@material-ui/icons/Add'
+import { ADD_TO_CART } from 'constants/index'
+import { CartContext } from 'App'
 
 const ProductCard = props => {
-  const { name, description, unitPrice, unitsInStock, image } = props
+  const { name, description, unitPrice, unitsInStock, image, productID } = props
+  const { dispatch } = useContext(CartContext)
   const classes = useStyles()
+
+  const addToCart = () => dispatch({ type: ADD_TO_CART, productId: productID })
+
   return (
     <Card className={classes.root}>
       <CardMedia className={classes.media} image={image} title={name} />
@@ -30,7 +36,11 @@ const ProductCard = props => {
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton className={classes.addIconButton} aria-label="add to favorites">
+        <IconButton
+          onClick={addToCart}
+          className={classes.addIconButton}
+          aria-label="add to favorites"
+        >
           <AddIcon />
         </IconButton>
       </CardActions>
@@ -43,6 +53,7 @@ ProductCard.propTypes = {
   description: PropTypes.string,
   image: PropTypes.string,
   unitPrice: PropTypes.number,
+  productID: PropTypes.number,
   unitsInStock: PropTypes.number,
 }
 
