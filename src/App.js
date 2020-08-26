e2e257713328ab1ac9e1afd930c906ec4a45f708
Rafael Buzatto de Campos <mightyrafa@gmail.com>
2020-08-25 22:48:11 -0300

    create context provider

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import NavBar from 'containers/NavBar'
 import Home from 'containers/Home'
 import Cart from 'containers/Cart'
 import TopProducts from 'containers/TopProducts'
 import Products from 'containers/Products'
+import useProducts from 'hooks/useProducts'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-const App = () => (
-  <Router>
-    <div className="app">
-      <NavBar />
-      <div className="container page">
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/products">
-            <Products />
-          </Route>
-          <Route path="/top-products">
-            <TopProducts />
-          </Route>
-          <Route path="/cart">
-            <Cart />
-          </Route>
-        </Switch>
-      </div>
-    </div>
-  </Router>
-)
+export const ProductsContext = createContext(null)
+
+const App = () => {
+  const [products] = useProducts()
+
+  return (
+    <ProductsContext.Provider value={{ products }}>
+      <Router>
+        <div className="app">
+          <NavBar />
+          <div className="container page">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/products">
+                <Products />
+              </Route>
+              <Route path="/top-products">
+                <TopProducts />
+              </Route>
+              <Route path="/cart">
+                <Cart />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    </ProductsContext.Provider>
+  )
+}
 export default App
