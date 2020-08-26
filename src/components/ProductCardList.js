ef5d9d9439eba550e6a8dcbd1453cda78f3c301c
Rafael Buzatto de Campos <mightyrafa@gmail.com>
2020-08-26 12:27:20 -0300

    add filter options to card List

--- src/components/ProductCardList.js
+++ src/components/ProductCardList.js
@@ -1,16 +1,34 @@
-import React, { useContext } from 'react'
+import React, { useContext, useCallback } from 'react'
+import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import ProductCard from './ProductCard'
 import { ProductsContext } from 'App'
 
-const ProductCardList = () => {
+const ProductCardList = ({ count, filter, order }) => {
   const classes = useStyles()
   const { products } = useContext(ProductsContext)
 
+  const filterProducts = useCallback(
+    () =>
+      products
+        .sort((p1, p2) => {
+          if (order === 'desc') {
+            return p1[filter] < p2[filter] ? 1 : -1
+          }
+          return p1[filter] > p2[filter] ? 1 : -1
+        })
+        .slice(0, count),
+    [products, filter, count, order],
+  )
+
   const renderProducts = () => {
     if (!products.length) return
-    return products.map(product => (
+    let productsList = products
+    if (filter) {
+      productsList = filterProducts()
+    }
+    return productsList.map(product => (
       <Grid item xs={12} sm={6} md={4} key={product.productID}>
         <ProductCard {...product} />
       </Grid>
@@ -23,6 +41,12 @@ const ProductCardList = () => {
       </Grid>
     </div>
   )
+}
+
+ProductCardList.propTypes = {
+  count: PropTypes.number,
+  filter: PropTypes.string,
+  order: PropTypes.string,
 }
 
 export default ProductCardList
