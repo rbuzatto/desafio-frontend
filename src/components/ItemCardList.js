6b76b6e1061f6b8f202f3c5f49d1ceb7943fb08a
Rafael Buzatto de Campos <mightyrafa@gmail.com>
2020-08-26 11:11:00 -0300

    addTotal in cart

--- src/components/ItemCardList.js
+++ src/components/ItemCardList.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import ItemCard from './ItemCard'
 import { makeStyles } from '@material-ui/core/styles'
 import List from '@material-ui/core/List'
+import Typography from '@material-ui/core/Typography'
 import { CartContext, ProductsContext } from 'App'
 
 const ItemCardList = () => {
@@ -10,17 +11,37 @@ const ItemCardList = () => {
   const { products } = useContext(ProductsContext)
   const { itemsIds, quantityById } = cart
 
+  const getTotalInCart = () => {
+    if (!products.length) return 0
+    return itemsIds.reduce((total, nextItemId) => {
+      const product = products.find(({ productID }) => productID === nextItemId)
+      return total + product.unitPrice * quantityById[nextItemId]
+    }, 0)
+  }
+
   const renderItems = () => {
     if (!products.length) return
     return itemsIds.map(id => <ItemCard productId={id} quantity={quantityById[id]} key={id} />)
   }
-  return <List className={classes.root}>{renderItems()}</List>
+  return (
+    <>
+      <List className={classes.root}>{renderItems()}</List>
+      <Typography className={classes.paddingGutter} variant="h5" color="textSecondary">
+        Total: ${getTotalInCart()}
+      </Typography>
+    </>
+  )
 }
 
 const useStyles = makeStyles(theme => ({
   root: {
     width: '100%',
     backgroundColor: theme.palette.background.paper,
+    borderBottom: '1px solid #e8e8e8',
+    marginBottom: 12,
+  },
+  paddingGutter: {
+    padding: '0 16px',
   },
 }))
 
