7300e4174d0548f3e98c40e928a64f40d2839fa1
Rafael Buzatto de Campos <mightyrafa@gmail.com>
2020-08-26 02:33:09 -0300

    remove max width

--- src/components/ItemCardList.js
+++ src/components/ItemCardList.js
@@ -18,7 +18,6 @@ const ItemCardList = () => {
 const useStyles = makeStyles(theme => ({
   root: {
     width: '100%',
-    maxWidth: 360,
     backgroundColor: theme.palette.background.paper,
   },
 }))
